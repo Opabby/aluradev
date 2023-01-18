8ce653a2ccfed9c9913df5efa345a0d69e714821
Thaís Rodeiro <opabby82@gmail.com>
2023-01-18 20:07:16 -0300

    adding hover effect

--- main.js
+++ main.js
@@ -33,3 +33,14 @@ searchButton.onclick = function open() {
         isOpen = false;
     }
 }
+
+const card = document.querySelector('.card');
+const cardIcons = document.querySelector('.card_icons');
+
+card.addEventListener('mouseover', () => {
+    cardIcons.classList.remove('hide');
+})
+
+card.addEventListener('mouseout', () => {
+    cardIcons.classList.add('hide');
+})
